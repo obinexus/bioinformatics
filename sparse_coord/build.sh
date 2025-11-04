8fe9a6a0cfb413235c44913a23ef7704098db4c5
OBINexus TDA Agent <tda@obinexus.org>
2025-11-04 09:04:20 +0000

    W

--- sparse_coord/build.sh
+++ sparse_coord/build.sh
@@ -1,1 +1,14 @@
-gcc -O2 -shared -fPIC plp128.c -o plp128.so
+# Most impoel via interipoon focomptueon formal defintion
+# when you complie you have to build it  uisn c 
+# whne you then need to run u need to  use adn interpion such as pyhton
+# iyou can buid  a compiel once as bllone one functor tha craa d interaplion layr 
+# which is apolyglto system teh polygmlot sytem wih whic asi inteion whill need to builde 
+#eithe via compiel  cbother aor intetio boobtsatwhion simualion o eucmuaion tehnbeaion
+# whn this this done the  ardion buidl with  htne buidl it an no proelm to sue carion ormpolar sysamte
+
+
+# BUILD OR COMPILE
+gcc -O2 -lm -o sparse_coord sparse_coord.c
+
+#RUN OR EXECUTE
+gcc -shared -fPIC -O2 -lm -o libsparse_coord.so sparse_coord.c
